a69c85f368672242468b6fc92caf4b7be8a34f4b
Babigdk <Babigdk@gmail.com>
2023-11-11 03:39:49 -0800

    Refactor the codebase from your previous task to reflect the new design

---
+++ battery/spindler_battery.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from battery import Battery
+
+class SpindlerBattery(Battery):
+    def __init__(self, last_service_date):
+        super().__init__(last_service_date)
+        self.current_date = datetime.today().date()
+
+    def needs_service(self):
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return service_threshold_date < self.current_date
